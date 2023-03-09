ec57fd8ee5cd54ae23799f80b2149c7ddd039029
AdamBear <adamxiong@126.com>
2023-03-09 17:58:10 +0800

    update

---
+++ proxy.py
@@ -0,0 +1,28 @@
+import requests
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    url = f'https://api.openai.com/{path}'
+    headers = {'User-Agent': request.headers.get('User-Agent')}
+    data = request.get_data()
+    resp = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=data,
+        cookies=request.cookies,
+        allow_redirects=False,
+        verify='cert.pem'
+    )
+    print("request", path)
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=88, ssl_context=('cert.pem', 'privkey.pem'))
